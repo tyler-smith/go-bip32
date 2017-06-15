05ee1c3199e1c22f4fc3bec7613d0a08b4b4ba8d
Tyler Smith <tylersmith.me@gmail.com>
2017-06-14 18:50:41 -0700

    TWEAK: Rearrange Key fields to use less memory.

--- bip32.go
+++ bip32.go
@@ -46,12 +46,12 @@ var (
 
 // Key represents a bip32 extended key
 type Key struct {
+	Key         []byte // 33 bytes
 	Version     []byte // 4 bytes
-	Depth       byte   // 1 bytes
 	ChildNumber []byte // 4 bytes
 	FingerPrint []byte // 4 bytes
 	ChainCode   []byte // 32 bytes
-	Key         []byte // 33 bytes
+	Depth       byte   // 1 bytes
 	IsPrivate   bool   // unserialized
 }
 
